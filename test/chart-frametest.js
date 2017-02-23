53c6a6f5107407dc83447f1604a93b3ca14a5535
tomgp <tom.g.pearson@gmail.com>
2017-02-23 15:13:11 +0000

    fix tests

--- test/chart-frametest.js
+++ test/chart-frametest.js
@@ -1,7 +1,7 @@
 var tape = require("tape");
 
 tape("chartFrame defaults", function(test) {
-	var chartFrame = require("../");
+	var chartFrame = require("../build/g-chartframe");
 	test.equal(chartFrame.frame().title(), 'Title: A description of the charts purpose');
 
 	test.end();
