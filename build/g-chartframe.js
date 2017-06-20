09ccad0b5f57e9b54b2b29fb2fa124132a76c43f
tomgp <tom.g.pearson@gmail.com>
2017-06-20 15:03:21 +0100

    add test that the module will work outside a browser environment (using JSDOM)

--- build/g-chartframe.js
+++ build/g-chartframe.js
@@ -251,6 +251,9 @@
               
             .call(attributeStyle, copyrightStyle);
   		}
+
+  // set chart margin.top
+
       if(containerClass === 'ft-webgraphic-s') {
         margin.top = (titlePosition.y + (titleLineCount * titleLineHeight) + (subtitleLineCount * subtitleLineHeight) + 24)
       } else if(containerClass === 'ft-webgraphic-m') {
@@ -260,8 +263,7 @@
       } else {
         margin.top = (titlePosition.y + (titleLineCount * titleLineHeight) + (subtitleLineCount * subtitleLineHeight) + (rem/3))
       }
-  // set chart margin.top
-  console.log(p.id)
+
   //watermark
 
   		p.selectAll('g.chart-watermark')
