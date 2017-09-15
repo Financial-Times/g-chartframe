9c5403e410ab3f521478e1d844b8e508db701ed0
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-09-15 12:17:44 +0100

    :heavy_plus_sign: Adds eslint and editorconfig settings, lints all the things.

--- src/webframe-m.js
+++ src/webframe-m.js
@@ -1,50 +1,50 @@
 import frame from './chartframe';
 
-function webFrameM(configObject){
-   var f = frame()
-    .autoPosition(true)
-    .containerClass('ft-webgraphic-m')
-    .backgroundColour('#FFF1E0')
-    .blackbar('#000')
-    .width(700)
-    .height(500)
-    // .watermark(watermarkPathDark)
-    // .watermarkSize(80)
-    // .watermarkOffset(-28)
-    .margin({bottom:104, right:5, left:20})
-    .rem(16)
-    .plotAdjuster(4)
-    .titleY(32)
-    .titleStyle({
-      'font-size':24,
-      'font-family': 'MetricWeb,sans-serif',
-      'font-weight': 400,
-      'fill':'#000',
-    })
-    .titleLineHeight(28)
-    .subtitleLineHeight(20)
-    .subtitleStyle({
-      'font-size':18,
-      'font-family': 'MetricWeb,sans-serif',
-      'fill': '#66605C',
-    })
-    .subtitleY(64)
-    .sourceLineHeight(16)
-    .sourcePlotYOffset(44)
-    .sourceStyle({
-      'font-size': '14px',
-      'font-family': 'MetricWeb,sans-serif',
-      'fill': '#66605C'
-    })
-    .copyrightStyle({
-      'font-size': '14px',
-      'font-style': 'italic',
-      'font-family': 'MetricWeb,sans-serif',
-      'fill': '#66605C',
-    });
+function webFrameM(configObject) {
+    const f = frame()
+        .autoPosition(true)
+        .containerClass('ft-webgraphic-m')
+        .backgroundColour('#FFF1E0')
+        .blackbar('#000')
+        .width(700)
+        .height(500)
+        // .watermark(watermarkPathDark)
+        // .watermarkSize(80)
+        // .watermarkOffset(-28)
+        .margin({ bottom: 104, right: 5, left: 20 })
+        .rem(16)
+        .plotAdjuster(4)
+        .titleY(32)
+        .titleStyle({
+            'font-size': 24,
+            'font-family': 'MetricWeb,sans-serif',
+            'font-weight': 400,
+            fill: '#000',
+        })
+        .titleLineHeight(28)
+        .subtitleLineHeight(20)
+        .subtitleStyle({
+            'font-size': 18,
+            'font-family': 'MetricWeb,sans-serif',
+            fill: '#66605C',
+        })
+        .subtitleY(64)
+        .sourceLineHeight(16)
+        .sourcePlotYOffset(44)
+        .sourceStyle({
+            'font-size': '14px',
+            'font-family': 'MetricWeb,sans-serif',
+            fill: '#66605C',
+        })
+        .copyrightStyle({
+            'font-size': '14px',
+            'font-style': 'italic',
+            'font-family': 'MetricWeb,sans-serif',
+            fill: '#66605C',
+        });
 
-  if(configObject !== undefined) f.attrs(configObject);
-  return f;
+    if (configObject !== undefined) f.attrs(configObject);
+    return f;
 }
 
 export default webFrameM;
--- test/chart-frametest.js
+++ test/chart-frametest.js
@@ -1,23 +1,21 @@
-const tape = require("tape");
-const fs = require("fs");
-const d3 = require("d3");
+const tape = require('tape');
+const fs = require('fs');
+const d3 = require('d3');
+const chartFrame = require('../build/g-chartframe');
+const jsdom = require('jsdom');
 
-tape("chartFrame defaults", function(test) {
-	const chartFrame = require("../build/g-chartframe");
-
-	test.equal(chartFrame.frame().title(), 'Title: A description of the charts purpose');
-	test.end();
+tape('chartFrame defaults', (test) => {
+    test.equal(chartFrame.frame().title(), 'Title: A description of the charts purpose');
+    test.end();
 });
 
-tape("chartFrame works outside browser", function(test) {
-  const jsdom = require('jsdom');
-  const { JSDOM } = jsdom;
-	const chartFrame = require("../build/g-chartframe");
-  const defaultFrame = chartFrame.frame();
-  const dom = new JSDOM(fs.readFileSync('test/scaffold.html'));
-  const chartContainer = d3.select(dom.window.document.querySelector('body div.chart-container'));
-  chartContainer.call(defaultFrame);
+tape('chartFrame works outside browser', (test) => {
+    const { JSDOM } = jsdom;
+    const defaultFrame = chartFrame.frame();
+    const dom = new JSDOM(fs.readFileSync('test/scaffold.html'));
+    const chartContainer = d3.select(dom.window.document.querySelector('body div.chart-container'));
+    chartContainer.call(defaultFrame);
 
-  test.equal(chartContainer.select('.chart-title tspan').text(), 'Title: A description of the charts purpose');
-	test.end();
+    test.equal(chartContainer.select('.chart-title tspan').text(), 'Title: A description of the charts purpose');
+    test.end();
 });
