c0b8ff3b81ca030ab2950bd98b8dc4befc7e2152
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-11-21 15:16:40 +0000

    Issue with save-svg-as-png module

--- src/chartframe.js
+++ src/chartframe.js
@@ -1,4 +1,4 @@
-import saveSvgAsPng from 'save-svg-as-png';
+import * as saveSvgAsPng from 'save-svg-as-png';
 import * as d3 from 'd3-selection';
 
 function chartFrame(configObject) {
@@ -609,7 +609,11 @@ function savePNG(svg, figure, scaleFactor) {
             }
         });
 
-    saveSvgAsPng(svg, 'area-chart.png', { scale: scaleFactor });
+    saveSvgAsPng(
+        svg,
+        `${figure.select('title').text().replace(/\s/g, '-').toLowerCase()}.png`,
+        { scale: scaleFactor },
+    );
 }
 
 export default chartFrame;
