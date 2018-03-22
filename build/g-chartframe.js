2dd705c1e1fdd71d45767e7747eca75e12ddf81c
Steve Bernard <steve.bernard@ft.com>
2018-03-22 17:43:30 +0000

    new video styles

--- build/g-chartframe.js
+++ build/g-chartframe.js
@@ -2091,22 +2091,19 @@
 	    bottom: 210,
 	    top: 233
 	  }).rem(48).titleX(207).titleY(130).titleLineHeight(68).titleStyle({
-	    'font-size': '68px',
+	    'font-size': '72px',
 	    fill: '#ffffff',
-	    'font-weight': 600,
-	    opacity: 0.9,
+	    'font-weight': 400,
 	    'font-family': 'MetricWeb,sans-serif'
 	  }).subtitleX(207).subtitleY(200).subtitleLineHeight(48).subtitleStyle({
 	    'font-size': '48px',
-	    fill: '#ffffff',
+	    fill: '#8e9095',
 	    'font-weight': 400,
-	    opacity: 0.7,
 	    'font-family': 'MetricWeb,sans-serif'
 	  }).sourceX(207).sourcePlotYOffset(60).sourceLineHeight(38).sourceStyle({
 	    'font-size': '36px',
-	    fill: '#ffffff',
+	    fill: '#8e9095',
 	    'font-weight': 400,
-	    opacity: 0.5,
 	    'font-family': 'MetricWeb,sans-serif'
 	  });
 	  if (configObject !== undefined) f.attrs(configObject);
