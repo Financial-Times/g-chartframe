389dc82fa8441450490eed94c614f630bf08d5ef
Ændrew Rininsland <andrew.rininsland@ft.com>
2018-11-14 16:48:12 +0000

    Reverting #54 because apparently all that bizarre and unnecessary code I removed actually needs to be there? 🤷🏼‍♀️

--- src/chartframe.js
+++ src/chartframe.js
@@ -3,9 +3,6 @@ import * as d3 from 'd3-selection';
 
 function chartFrame(configObject) {
     let autoPosition = false;
-    let a11yDesc = 'A graphic by the Financial Times';
-    let a11yPlotPresentation = true;
-    let a11yTitle = 'A chart';
     let backgroundColour;
     let containerClass = 'g-chartframe';
     let copyright = '© FT';
@@ -49,12 +46,8 @@ function chartFrame(configObject) {
     const titlePosition = { x: 1, y: 30 };
     const transition = 0.2;
     const convertFrom = {
-        mm(x) {
-            return x * 2.83464480558843;
-        },
-        px(x) {
-            return x;
-        },
+        mm(x) { return (x * 2.83464480558843); },
+        px(x) { return x; },
     };
     const custom = {};
 
@@ -66,41 +59,20 @@ function chartFrame(configObject) {
 
     function frame(p) {
         // overall graphic properties
-        p.attr('class', containerClass).attr(
-            'font-family',
-            'MetricWeb,sans-serif',
-        );
-
-        p.attr('role', 'img');
-
+        p.attr('class', containerClass)
+            .attr('font-family', 'MetricWeb,sans-serif');
         if (p.node().nodeName.toLowerCase() === 'svg') {
             p.transition(transition)
                 .attr('width', graphicWidth)
                 .attr('height', graphicHeight)
-                .attr(
-                    'viewBox',
-                    ['0 0', graphicWidth, graphicHeight].join(' '),
-                );
+                .attr('viewBox', ['0 0', graphicWidth, graphicHeight].join(' '));
 
-            if (a11yTitle !== false || title !== false) {
-                p.append('title')
-                    .text(a11yTitle || title)
-                    .attr('id', `${containerClass}__chart-a11y-title`);
-                p.attr(
-                    'aria-labelledby',
-                    `${containerClass}__chart-a11y-title`,
-                );
-            }
+            p.selectAll('title')
+                .data([title])
+                .enter()
+                .append('title');
 
-            if (a11yDesc !== false) {
-                p.append('desc')
-                    .text(a11yDesc)
-                    .attr('id', `${containerClass}__chart-a11y-desc`);
-                p.attr(
-                    'aria-labelledby',
-                    `${p.attr('aria-labelledby') ? `${p.attr('aria-labelledby')} ` : ''}${containerClass}__chart-a11y-desc`,
-                );
-            }
+            p.selectAll('title').text(title);
         }
 
         // background
@@ -109,7 +81,6 @@ function chartFrame(configObject) {
                 .data([backgroundColour])
                 .enter()
                 .append('rect')
-                .attr('role', 'presentation')
                 .attr('id', 'chart-background')
                 .attr('class', 'chart-background');
 
@@ -171,16 +142,12 @@ function chartFrame(configObject) {
                 .attr('class', 'chart-title')
                 .attr('id', `${containerClass}title`)
                 .call((titleText) => {
-                    titleText
-                        .selectAll('tspan')
+                    titleText.selectAll('tspan')
                         .data(title.split('|'))
                         .enter()
                         .append('tspan')
                         .html(d => d)
-                        .attr(
-                            'y',
-                            (d, i) => titlePosition.y + i * titleLineHeight,
-                        )
+                        .attr('y', (d, i) => (titlePosition.y + (i * titleLineHeight)))
                         .attr('x', titlePosition.x)
                         .call(attributeStyle, titleStyle);
                 });
@@ -188,7 +155,7 @@ function chartFrame(configObject) {
             p.selectAll('text.chart-title tspan')
                 .html(d => d)
                 .transition(transition)
-                .attr('y', (d, i) => titlePosition.y + i * titleLineHeight)
+                .attr('y', (d, i) => (titlePosition.y + (i * titleLineHeight)))
                 .attr('x', titlePosition.x)
                 .call(attributeStyle, titleStyle);
         }
@@ -202,8 +169,7 @@ function chartFrame(configObject) {
                 .attr('id', `${containerClass}subtitle`)
                 .attr('class', 'chart-subtitle')
                 .call((subtitleText) => {
-                    subtitleText
-                        .selectAll('tspan')
+                    subtitleText.selectAll('tspan')
                         .data(subtitle.split('|'))
                         .enter()
                         .append('tspan')
@@ -211,13 +177,9 @@ function chartFrame(configObject) {
                         .attr('id', `${containerClass}subtitle`)
                         .attr('y', (d, i) => {
                             if (titleLineCount > 1) {
-                                return (
-                                    titlePosition.y +
-                                    titleLineCount * titleLineHeight +
-                                    subtitleLineHeight * i
-                                );
+                                return (titlePosition.y + (titleLineCount * titleLineHeight) + (subtitleLineHeight * i));
                             }
-                            return subtitlePosition.y + i * subtitleLineHeight;
+                            return (subtitlePosition.y + (i * subtitleLineHeight));
                         })
 
                         .attr('x', subtitlePosition.x)
@@ -229,13 +191,9 @@ function chartFrame(configObject) {
                 .transition(transition)
                 .attr('y', (d, i) => {
                     if (titleLineCount > 1) {
-                        return (
-                            titlePosition.y +
-                            titleLineCount * titleLineHeight +
-                            subtitleLineHeight * i
-                        );
+                        return (titlePosition.y + (titleLineCount * titleLineHeight) + (subtitleLineHeight * i));
                     }
-                    return subtitlePosition.y + i * subtitleLineHeight;
+                    return (subtitlePosition.y + (i * subtitleLineHeight));
                 })
                 .attr('x', subtitlePosition.x)
                 .call(attributeStyle, subtitleStyle);
@@ -250,8 +208,7 @@ function chartFrame(configObject) {
                 .attr('class', 'chart-source')
                 .attr('id', `${containerClass}source`)
                 .call((sourceText) => {
-                    sourceText
-                        .selectAll('tspan')
+                    sourceText.selectAll('tspan')
                         .data(source.split('|'))
                         .enter()
                         .append('tspan')
@@ -259,14 +216,9 @@ function chartFrame(configObject) {
                         .attr('id', `${containerClass}source`)
                         .attr('y', (d, i) => {
                             if (sourcePosition.y) {
-                                return sourcePosition.y + i * sourceLineHeight;
+                                return (sourcePosition.y + (i * sourceLineHeight));
                             }
-                            return (
-                                graphicHeight -
-                                (margin.bottom - sourcePlotYOffset) +
-                                sourceLineHeight * 1.5 +
-                                i * sourceLineHeight
-                            );
+                            return ((graphicHeight - (margin.bottom - sourcePlotYOffset) + sourceLineHeight * 1.5) + ((i) * sourceLineHeight)); // eslint-disable-line
                         })
                         .attr('x', subtitlePosition.x)
                         .call(attributeStyle, subtitleStyle);
@@ -277,18 +229,14 @@ function chartFrame(configObject) {
                 .transition(transition)
                 .attr('y', (d, i) => {
                     if (sourcePosition.y) {
-                        return sourcePosition.y + i * sourceLineHeight;
+                        return (sourcePosition.y + (i * sourceLineHeight));
                     }
-                    return (
-                        graphicHeight -
-                        (margin.bottom - sourcePlotYOffset) +
-                        sourceLineHeight * 1.5 +
-                        i * sourceLineHeight
-                    );
+                    return ((graphicHeight - (margin.bottom - sourcePlotYOffset) + sourceLineHeight * 1.5) + ((i) * sourceLineHeight)); // eslint-disable-line
                 })
                 .attr('x', sourcePosition.x)
                 .call(attributeStyle, sourceStyle);
         }
+
 
         // copyright
         if (copyrightStyle) {
@@ -302,42 +250,21 @@ function chartFrame(configObject) {
                 .attr('x', sourcePosition.x)
                 .attr('y', () => {
                     if (sourceLineCount > 1) {
-                        return (
-                            graphicHeight -
-                            (margin.bottom - sourcePlotYOffset) +
-                            sourceLineHeight * 1.125 +
-                            sourceLineCount * sourceLineHeight * 1.2
-                        );
+                        return (graphicHeight - (margin.bottom - sourcePlotYOffset) + (sourceLineHeight * 1.125) + (sourceLineCount * sourceLineHeight * 1.2)); // eslint-disable-line
                     }
-                    return (
-                        graphicHeight -
-                        (margin.bottom - sourcePlotYOffset) +
-                        sourceLineHeight * 2.5
-                    );
+                    return (graphicHeight - (margin.bottom - sourcePlotYOffset) + (sourceLineHeight * 2.5)); // eslint-disable-line
                 })
+
 
                 .call(attributeStyle, copyrightStyle);
         }
 
+
         // TODO figure out a way to improve this autoPosition stuff, needs ot be configurable so we don't have to reference specific classes
-        if (
-            autoPosition &&
-            (containerClass === 'ft-printgraphic' ||
-                containerClass === 'ft-socialgraphic' ||
-                containerClass === 'ft-videographic')
-        ) {
-            margin.top =
-                titlePosition.y +
-                titleLineCount * titleLineHeight +
-                subtitleLineCount * subtitleLineHeight +
-                rem / 3;
+        if (autoPosition && (containerClass === 'ft-printgraphic' || containerClass === 'ft-socialgraphic' || containerClass === 'ft-videographic')) {
+            margin.top = (titlePosition.y + (titleLineCount * titleLineHeight) + (subtitleLineCount * subtitleLineHeight) + (rem / 3));
         } else if (autoPosition) {
-            margin.top =
-                titlePosition.y +
-                titleLineCount * titleLineHeight +
-                subtitleLineCount * subtitleLineHeight +
-                28 -
-                plotAdjuster;
+            margin.top = (titlePosition.y + (titleLineCount * titleLineHeight) + (subtitleLineCount * subtitleLineHeight) + 28 - plotAdjuster); // eslint-disable-line
         }
 
         // watermark
@@ -348,36 +275,26 @@ function chartFrame(configObject) {
             .append('g')
             .attr('class', 'chart-watermark')
             .html(watermarkMarkup)
-            .attr('role', 'presentation')
-            .attr(
-                'transform',
-                `translate(${graphicWidth -
-                    watermarkWidth -
-                    watermarkOffsetX},${graphicHeight -
-                    watermarkHeight -
-                    watermarkOffsetY}) scale(1) `,
-            );
+            .attr('transform', `translate(${graphicWidth - watermarkWidth - watermarkOffsetX},${graphicHeight - watermarkHeight - watermarkOffsetY}) scale(1) `);
 
         p.selectAll('g.chart-watermark')
             .html(watermarkMarkup)
             .transition()
-            .attr(
-                'transform',
-                `translate(${graphicWidth -
-                    watermarkWidth -
-                    watermarkOffsetX},${graphicHeight -
-                    watermarkHeight -
-                    watermarkOffsetY}) scale(1) `,
-            );
+            .attr('transform', `translate(${graphicWidth - watermarkWidth - watermarkOffsetX},${graphicHeight - watermarkHeight - watermarkOffsetY}) scale(1) `);
 
         // plot area (where you put the chart itself)
-        plot = p.append('g')
+        p.selectAll('g.chart-plot')
+            .data([0])
+            .enter()
+            .append('g')
             .attr('class', 'chart-plot')
             .attr('transform', `translate(${margin.left},${margin.top})`);
 
-        if (a11yPlotPresentation) {
-            plot.attr('role', 'presentation');
-        }
+        plot = p.selectAll('g.chart-plot');
+
+        plot.transition(transition)
+            .duration(0)
+            .attr('transform', `translate(${margin.left},${margin.top})`);
 
         if (showDownloadPngButtons) {
             let parent;
@@ -389,19 +306,15 @@ function chartFrame(configObject) {
 
             // Prevent this from being rendered twice
             if (parent.selectAll('.button-holder').size() === 0) {
-                const holder = parent
-                    .append('div')
-                    .attr('class', 'button-holder');
-                holder
-                    .append('button')
+                const holder = parent.append('div').attr('class', 'button-holder');
+                holder.append('button')
                     .attr('class', 'save-png-button save-png-button__1x')
                     .text('Save as .png')
                     .style('float', 'left')
                     .style('opacity', 0.6)
                     .on('click', () => savePNG(p, 1));
 
-                holder
-                    .append('button')
+                holder.append('button')
                     .attr('class', 'save-png-button save-png-button__2x')
                     .style('float', 'left')
                     .style('opacity', 0.6)
@@ -411,32 +324,8 @@ function chartFrame(configObject) {
         }
     }
 
+
     // Setters and getters
-    frame.a11y = ({ title: newTitle, desc: newDesc } = {}) => {
-        if (newTitle !== undefined) a11yTitle = newTitle;
-        if (newDesc !== undefined) a11yDesc = newDesc;
-        if (newTitle === undefined && newDesc === undefined) return { title: a11yTitle, desc: a11yDesc };
-
-        return frame;
-    };
-
-    frame.a11yDesc = (x) => {
-        if (x === undefined) return a11yDesc;
-        a11yDesc = x;
-        return frame;
-    };
-
-    frame.a11yPlotPresentation = (x) => {
-        if (x === undefined) return a11yPlotPresentation;
-        a11yPlotPresentation = x;
-        return frame;
-    };
-
-    frame.a11yTitle = (x) => {
-        if (x === undefined) return a11yTitle;
-        a11yTitle = x;
-        return frame;
-    };
 
     frame.autoPosition = (x) => {
         if (x === undefined) return autoPosition;
@@ -691,48 +580,41 @@ function chartFrame(configObject) {
 
     frame.attrs = (x) => {
         if (x === undefined) {
-            return Object.assign(
-                {},
-                {
-                    a11yDesc,
-                    a11yPlotPresentation,
-                    a11yTitle,
-                    autoPosition,
-                    // axisAlign, // @FIX This is undef?
-                    containerClass,
-                    copyright,
-                    copyrightStyle,
-                    blackbar,
-                    goalposts,
-                    graphicHeight,
-                    graphicWidth,
-                    margin,
-                    plot,
-                    plotAdjuster,
-                    rem,
-                    subtitle,
-                    subtitleLineHeight,
-                    subtitlePosition,
-                    subtitleStyle,
-                    source,
-                    sourceLineHeight,
-                    sourcePosition,
-                    sourceStyle,
-                    title,
-                    titleLineHeight,
-                    titlePosition,
-                    titleStyle,
-                    watermarkLocation,
-                    watermarkMarkup,
-                    watermarkOffsetX,
-                    watermarkOffsetY,
-                    watermarkHeight,
-                    watermarkWidth,
-                    whitebar,
-                    units,
-                },
-                custom,
-            );
+            return Object.assign({}, {
+                autoPosition,
+                // axisAlign, // @FIX This is undef?
+                containerClass,
+                copyright,
+                copyrightStyle,
+                blackbar,
+                goalposts,
+                graphicHeight,
+                graphicWidth,
+                margin,
+                plot,
+                plotAdjuster,
+                rem,
+                subtitle,
+                subtitleLineHeight,
+                subtitlePosition,
+                subtitleStyle,
+                source,
+                sourceLineHeight,
+                sourcePosition,
+                sourceStyle,
+                title,
+                titleLineHeight,
+                titlePosition,
+                titleStyle,
+                watermarkLocation,
+                watermarkMarkup,
+                watermarkOffsetX,
+                watermarkOffsetY,
+                watermarkHeight,
+                watermarkWidth,
+                whitebar,
+                units,
+            }, custom);
         }
 
         Object.keys(x).forEach((setterName) => {
@@ -753,10 +635,7 @@ function chartFrame(configObject) {
 
 function isFunction(functionToCheck) {
     const getType = {};
-    return (
-        functionToCheck &&
-        getType.toString.call(functionToCheck) === '[object Function]'
-    );
+    return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
 }
 
 const classes = [
@@ -788,27 +667,20 @@ const classes = [
 ];
 
 function savePNG(svg, scaleFactor) {
-    svg.selectAll(classes.join(', ')).each(function inlineProps() {
-        const element = this;
-        const computedStyle = getComputedStyle(element, null);
+    svg.selectAll(classes.join(', '))
+        .each(function inlineProps() {
+            const element = this;
+            const computedStyle = getComputedStyle(element, null);
 
-        // loop through and compute inline svg styles
-        for (let i = 0; i < computedStyle.length; i += 1) {
-            const property = computedStyle.item(i);
-            const value = computedStyle.getPropertyValue(property);
-            element.style[property] = value;
-        }
-    });
+            // loop through and compute inline svg styles
+            for (let i = 0; i < computedStyle.length; i += 1) {
+                const property = computedStyle.item(i);
+                const value = computedStyle.getPropertyValue(property);
+                element.style[property] = value;
+            }
+        });
 
-    saveSvgAsPng(
-        svg.node(),
-        `${svg
-            .select('title')
-            .text()
-            .replace(/\s/g, '-')
-            .toLowerCase()}.png`,
-        { scale: scaleFactor },
-    );
+    saveSvgAsPng(svg.node(), `${svg.select('title').text().replace(/\s/g, '-').toLowerCase()}.png`, { scale: scaleFactor });
 }
 
 export default chartFrame;
