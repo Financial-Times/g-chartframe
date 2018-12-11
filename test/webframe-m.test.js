8938c89102403b385d4d372701a0550c8b207805
Ændrew Rininsland <andrew.rininsland@ft.com>
2018-12-11 17:22:11 +0000

    Unix is case sensitive innit?

--- test/webframe-m.test.js
+++ test/webframe-m.test.js
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as d3 from 'd3-selection';
 import 'd3-transition';
 import jsdom from 'jsdom';
-import webframeM from '../src/webframe-M';
+import webframeM from '../src/webframe-m';
 
 test('webframe-m renders as expected with defaults', () => {
     const { JSDOM } = jsdom;
